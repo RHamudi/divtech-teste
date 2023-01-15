9172ca91942485b22e041e507368e19f2bb94d37
Ramon Ramos <ramonramos.silva16@gmail.com>
2023-01-15 08:31:27 -0300

    feat: migrations

---
+++ Migrations/20230115104654_FirstMigration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace divtechteste.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Fornecedores",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "NVARCHAR(100)", maxLength: 100, nullable: false),
+                    CNPJ = table.Column<string>(type: "NVARCHAR(14)", maxLength: 14, nullable: false),
+                    Especialidade = table.Column<string>(type: "NVARCHAR", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fornecedores", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Fornecedores");
+        }
+    }
+}
--- Models/Fornecedores.cs
+++ Models/Fornecedores.cs
@@ -7,7 +7,7 @@ namespace divtech_teste.Models
 {
     public class Fornecedores
     {
-        public string ID { get; set; }
+        public int ID { get; set; }
         public string Nome { get; set; }
         public string CNPJ { get; set; }
         public string Especialidade { get; set; }
