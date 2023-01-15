ff423f08c8571210782cb571f9cb5d2166c483e0
Ramon Ramos <ramonramos.silva16@gmail.com>
2023-01-15 07:09:08 -0300

    feat: create model fornecedores

---
+++ Models/Fornecedores.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace divtech_teste.Models
+{
+    public class Fornecedores
+    {
+        public string ID { get; set; }
+        public string Nome { get; set; }
+        public string CNPJ { get; set; }
+        public string Especialidade { get; set; }
+    }
+}
